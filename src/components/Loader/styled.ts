42b37a8156258422e6266cce1be246c3af8dbfa2
Vladislav Shkutovich <vladislav.shkutovich@gmail.com>
2022-12-07 11:09:29 +0300

    style: loader changes

--- src/components/Loader/styled.ts
+++ src/components/Loader/styled.ts
@@ -11,16 +11,15 @@ const spinnerAnimation = keyframes`
 
 export const StyledLoader = styled.div`
 	position: absolute;
-	left: 50vw;
+	left: 45vw;
 	top: 50vh;
-	display: flex;
-	align-self: center;
-	justify-self: center;
+	margin: 0;
+	padding: 0;
 	z-index: 10;
 	border: 0.9rem solid ${({ theme }) => theme.colors.helperBlue3};
 	border-top: 0.9rem solid ${({ theme }) => theme.colors.primary};
 	border-radius: 50%;
-	width: 7rem;
-	height: 7rem;
+	width: 8rem;
+	height: 8rem;
 	animation: ${spinnerAnimation} 0.8s linear infinite;
 `
